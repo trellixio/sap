9c944f5b7a97c0cad6cbc7026847a4d5617ce64c
yandah <yan@trybeans.com>
2023-03-12 11:57:05 +0100

    fix typing

--- sap/beanie/__init__.py
+++ sap/beanie/__init__.py
@@ -4,8 +4,8 @@ Beanie.
 This package regroups all common helpers exclusively for Beanie.
 Learn more about https://github.com/roman-right/beanie
 """
-from .models import DocMeta, Link
 from .document import Document
+from .models import DocMeta, Link
 
 __all__ = [
     "DocMeta",
