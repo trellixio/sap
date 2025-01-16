2b1e4b4385863d43c7422a21e734286fb7bdf559
Yan Dah <yan@trybeans.com>
2025-01-16 14:53:27 +0100

    version 0.3.5: fix access to instance

--- sap/fastapi/serializers.py
+++ sap/fastapi/serializers.py
@@ -29,7 +29,7 @@ from typing_extensions import Literal
 
 from fastapi import Request
 from pydantic import BaseModel
-from pydantic.fields import FieldInfo
+from pydantic.fields import FieldInfo, PrivateAttr
 
 from sap.beanie.document import Document
 from sap.sqlachemy import AlchemyOrPydanticModelT
@@ -155,7 +155,7 @@ SerializerT = TypeVar("SerializerT", bound=ObjectSerializer[Any])
 class WriteObjectSerializer(BaseModel, Generic[AlchemyOrPydanticModelT]):
     """Serialize an object for create or update."""
 
-    _instance: Optional[AlchemyOrPydanticModelT] = None
+    _instance: Optional[AlchemyOrPydanticModelT] = PrivateAttr(default=None)
     embedded_serializers: ClassVar[dict[str, type["WriteObjectSerializer[Any]"]]] = {}
 
     @property
@@ -168,15 +168,21 @@ class WriteObjectSerializer(BaseModel, Generic[AlchemyOrPydanticModelT]):
         """Set the instance."""
         self._instance = instance
 
-    def __init__(self, **data: Any) -> None:
+    def __init__(self, instance: Optional[AlchemyOrPydanticModelT] = None, **data: Any) -> None:
         """Override init to filter embedded serializers."""
         super().__init__(**data)
+        self._instance = instance
         for field_name, field_info in self.model_fields.items():
             if inspect.isclass(field_info.annotation) and issubclass(field_info.annotation, WriteObjectSerializer):
                 self.embedded_serializers[field_name] = field_info.annotation
 
     @overload
     async def run_async_validators(self, *, db: "AsyncSession", **kwargs: Any) -> None:
+        """run_async_validators overload for sqlachemy app using db."""
+        ...
+
+    @overload
+    async def run_async_validators(self, **kwargs: Any) -> None:
         """run_async_validators overload for sqlachemy app using db."""
         ...
 
@@ -187,8 +193,8 @@ class WriteObjectSerializer(BaseModel, Generic[AlchemyOrPydanticModelT]):
         field_serializer: WriteObjectSerializer[AlchemyOrPydanticModelT]
         for field_name in self.embedded_serializers:
             if field_serializer := getattr(self, field_name):
-                if self._instance:
-                    field_serializer._instance = getattr(self._instance, field_name)
+                if self.instance:
+                    field_serializer.instance = getattr(self.instance, field_name)
                 await field_serializer.run_async_validators(**kwargs)
 
     def model_dump(
@@ -242,7 +248,7 @@ class WriteObjectSerializer(BaseModel, Generic[AlchemyOrPydanticModelT]):
         for field_name, field_model in self.embedded_serializers.items():
             if not result[field_name]:
                 continue
-            if instance_embedded := getattr(self._instance, field_name, None):
+            if instance_embedded := getattr(self.instance, field_name, None):
                 result[field_name] = instance_embedded.model_copy(update=result[field_name])
             else:
                 result[field_name] = field_model(**result[field_name])
@@ -254,9 +260,14 @@ class WriteObjectSerializer(BaseModel, Generic[AlchemyOrPydanticModelT]):
         """create overload for sqlachemy app using db."""
         ...
 
+    @overload
+    async def create(self, **kwargs: Any) -> AlchemyOrPydanticModelT:
+        """create overload for sqlachemy app using db."""
+        ...
+
     async def create(self, **kwargs: Any) -> AlchemyOrPydanticModelT:
         """Create the object in the database using the data extracted by the serializer."""
-        instance_class: type[AlchemyOrPydanticModelT] | None = self.model_fields["_instance"].annotation
+        instance_class: type[AlchemyOrPydanticModelT] | None = self.model_fields["instance"].annotation
         if instance_class and issubclass(instance_class, Document):
             return await instance_class(**self.model_dump()).create()
         raise NotImplementedError
@@ -266,15 +277,20 @@ class WriteObjectSerializer(BaseModel, Generic[AlchemyOrPydanticModelT]):
         """update overload for sqlachemy app using db."""
         ...
 
+    @overload
+    async def update(self, **kwargs: Any) -> AlchemyOrPydanticModelT:
+        """update overload for sqlachemy app using db."""
+        ...
+
     async def update(self, **kwargs: Any) -> AlchemyOrPydanticModelT:
         """Update the object in the database using the data extracted by the serializer."""
-        assert self._instance
+        assert self.instance
 
-        if isinstance(self._instance, Document):
-            self._instance = self._instance.model_copy(update=self.model_dump())
-            assert self._instance and isinstance(self._instance, Document)
-            await self._instance.save()
-            return self._instance
+        if isinstance(self.instance, Document):
+            self.instance = self.instance.model_copy(update=self.model_dump())
+            assert self.instance and isinstance(self.instance, Document)
+            await self.instance.save()
+            return self.instance
 
         raise NotImplementedError
 
